b98570e5205eddc2d843bc922577f9e4edfae7fb
rhsin <ryanhsin@ymail.com>
2023-02-24 22:14:09 -0800

    Add comments for clarification

--- wix-visitors.js
+++ wix-visitors.js
@@ -2,10 +2,15 @@ import { fetch } from 'wix-fetch';
 import wixData from 'wix-data';
 import wixWindow from 'wix-window';
 
+// API key goes here
 const apiKeyIPG = 'f765d915284e4dff87928acc36c4b153';
 
 const url_IPG = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKeyIPG}`;
 
+// The collection's permissions settings must be set as -> Can add content: Anyone.
+// Copy and paste this whole code snippet into the page you want tracked, with Dev Mode enabled.
+
+// This needs to match the name of the collection in Wix Database.
 const collectionName = 'Visitors';
 
 $w.onReady(function () {
